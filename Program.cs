48eb31f0bb1b4266417bc3e299a7f13d40c58e0b
nataytali <94608278+nataytali@users.noreply.github.com>
2021-12-08 12:01:22 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExceptionTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = new string[5];
+            list[0] = "Sunday";
+            list[1] = "Monday";
+            list[2] = "Tuesday";
+            list[3] = "Wednesday";
+            list[4] = "Thursday";
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(list[i].ToString());
+                }
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
